39a06071962833015472676a4980a941e90cdba4
ed0419 <genden80@gmail.com>
2022-08-22 20:14:14 +0800

    Add Debug Form Update 2

--- SQLGame101/SQLGame101/Form1.Designer.cs
+++ SQLGame101/SQLGame101/Form1.Designer.cs
@@ -30,88 +30,8 @@ namespace SQLGame101
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
             this.DebugBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("新細明體", 16F);
-            this.label1.Location = new System.Drawing.Point(236, 375);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 22);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "show";
-            // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("新細明體", 12F);
-            this.button1.Location = new System.Drawing.Point(47, 342);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(152, 55);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Start";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 12;
-            this.listBox1.Location = new System.Drawing.Point(47, 32);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(242, 148);
-            this.listBox1.TabIndex = 2;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Font = new System.Drawing.Font("新細明體", 18F);
-            this.textBox1.Location = new System.Drawing.Point(48, 298);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(150, 36);
-            this.textBox1.TabIndex = 3;
-            this.textBox1.Text = "COM5";
-            // 
-            // button2
-            // 
-            this.button2.Enabled = false;
-            this.button2.Location = new System.Drawing.Point(50, 409);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(148, 32);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "Disconnect";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // serialPort1
-            // 
-            this.serialPort1.PortName = "COM5";
-            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
-            // 
-            // textBox2
-            // 
-            this.textBox2.Font = new System.Drawing.Font("新細明體", 18F);
-            this.textBox2.Location = new System.Drawing.Point(312, 46);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(453, 36);
-            this.textBox2.TabIndex = 5;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(325, 137);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(33, 12);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "label2";
             // 
             // DebugBtn
             // 
@@ -129,31 +49,14 @@ namespace SQLGame101
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.DebugBtn);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button2;
-        private System.IO.Ports.SerialPort serialPort1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button DebugBtn;
     }
 }
--- SQLGame101/SQLGame101/Form1.cs
+++ SQLGame101/SQLGame101/Form1.cs
@@ -18,82 +18,9 @@ namespace SQLGame101
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-
-            if (!serialPort1.IsOpen)
-            {
-                try
-                {
-                    serialPort1.PortName = textBox1.Text;
-                    serialPort1.Open();
-                    label1.Text = "start read";
-                    button1.Enabled = false;
-                    button2.Enabled = true;
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message);
-                }
-            }
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                if (serialPort1.IsOpen)
-                {
-                    serialPort1.Close();
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            label1.Text = "stoped";
-            button1.Enabled = true;
-            button2.Enabled = false;
-        }
-
-        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
-        {
-            string recvtext = serialPort1.ReadLine();
-            setText(recvtext);
-        }
-        private void setText(string val)
-        {
-            if (this.textBox2.InvokeRequired)
-            {
-                serialCallback scb = new serialCallback(setText);
-                this.Invoke(scb, new object[] { val });
-            }
-            else
-            {
-                textBox2.Text = val;
-
-            }
-        }
-
         private void Form1_Load(object sender, EventArgs e)
         {
-            using (var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_PnPEntity WHERE Manufacturer!=NULL AND Manufacturer like 'Arduino%'"))
-            {
-                var portnames = SerialPort.GetPortNames();
-                var ports = searcher.Get().Cast<ManagementBaseObject>().ToList().Select(p => p["Caption"].ToString());
-                //var portList = portnames.Select(n => portnames.SingleOrDefault(s => n.Contains(s))).ToList();
-                var tmp = string.Join(",", ports.ToArray());
-                foreach (string n in portnames)
-                {
-                    if (tmp.Contains(n))
-                    {
-                        listBox1.Items.Add(n);
-                        textBox1.Text = n;
-                    }
-                }
-            }
+            
 
 
 
@@ -102,6 +29,7 @@ namespace SQLGame101
         private void DebugBtn_Click(object sender, EventArgs e)
         {
             var form = new Form2();
+            this.Hide();
             form.Show();
             
         }
