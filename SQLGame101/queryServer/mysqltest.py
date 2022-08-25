75244ff60b9504df9e42c9b7485994762a0de0b9
ed0419 <genden80@gmail.com>
2022-08-25 16:12:17 +0800

    sql api

--- SQLGame101/queryServer/mysqltest.py
+++ SQLGame101/queryServer/mysqltest.py
@@ -1,5 +1,5 @@
 import pymysql
-db = pymysql.connect(host='steal.tyc4d.tw',user='user',passwd="zxc19201080",db='mydb')
+db = pymysql.connect(host='steal.tyc4d.tw',user='user',passwd="u",db='mydb')
 cursor = db.cursor()
 cursor.execute("SELECT * from users")
 result = cursor.fetchall()
